4a29094a946be1125e28ba43aa80b89a6320011e
JJONK19 <josuejaviera19@gmail.com>
2020-10-07 21:16:30 -0600

    Tab v2

--- Othell/Othell/Tablero - V.Dinamico.aspx
+++ Othell/Othell/Tablero - V.Dinamico.aspx
@@ -5,12 +5,40 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<style type="text/css">
+.tab {
+        width: 580px;
+        z-index: 1;
+        left: 510px;
+        top: 33px;
+        position: absolute;
+        height: 580px;
+        right: -10px;
+        background-color: #83CC70;
+        text-align: center;
+        
+    }
+
+td, tr{
+    width: 66px;
+    height: 66px;
+
+}
+
+.but{
+    border-radius: 80%;
+    width: 60px;
+    height: 60px;
+    border-color:#83CC70;
+    background-color:#83CC70;
+}
+</style>
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
+        <asp:Table ID="tablero" runat="server" CssClass ="tab">
+        </asp:Table>
     </form>
 </body>
 </html>
--- Othell/Othell/Tablero - V.Dinamico.aspx.cs
+++ Othell/Othell/Tablero - V.Dinamico.aspx.cs
@@ -11,7 +11,58 @@ namespace Othell
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                if(Session["Tablero"] == null)
+                {
+                    Button[,] bot = new Button[50,50];
+                    for(int i = 0; i < 50; i++)
+                    {
+                        for (int j = 0; j < 50; j++)
+                        {
+                            Button b = new Button();
+                            b.ID = (char)(65 + j) + (i + 1).ToString();
+                            b.Text = "";
+                            b.Attributes.Add("class", "but");
+                            bot[i, j] = b;
+                        }
+                    }
+                    Session["Tablero"] = bot;
+                    TableRow f;
+                    TableCell c;
 
+                    for(int k = 0; k<50; k++)
+                    {
+                        f = new TableRow();
+                        for(int l =0; l<50; l++)
+                        {
+                            c = new TableCell();
+                            c.Controls.Add(bot[k, l]);
+                            f.Cells.Add(c);
+                        }
+                        tablero.Rows.Add(f);
+                    }
+                }
+            }
+
+            if (Page.IsPostBack)
+            {
+                Button[,] bot = (Button[,])Session["Tablero"];
+                TableRow f;
+                TableCell c;
+
+                for (int k = 0; k < 50; k++)
+                {
+                    f = new TableRow();
+                    for (int l = 0; l < 50; l++)
+                    {
+                        c = new TableCell();
+                        c.Controls.Add(bot[k, l]);
+                        f.Cells.Add(c);
+                    }
+                    tablero.Rows.Add(f);
+                }
+            }
         }
     }
 }
